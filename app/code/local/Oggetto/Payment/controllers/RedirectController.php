f04e029b60fef9248dd7268b4bcf4df75435455c
tylerbates <tylerbates098@gmail.com>
2014-08-14 13:16:39 +0400

    add payment method structure
    [re #edu_homework_5]

---
+++ app/code/local/Oggetto/Payment/controllers/RedirectController.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Oggetto extension for Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade
+ * the Oggetto Payment module to newer versions in the future.
+ * If you wish to customize the Oggetto Payment module for your needs
+ * please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category   Oggetto
+ * @package    Oggetto_Questions
+ * @copyright  Copyright (C) 2014 Oggetto Web ltd (http://oggettoweb.com/)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Payment redirect controller
+ *
+ * @category   Oggetto
+ * @package    Oggetto_Payment
+ * @subpackage controllers
+ * @author     Andrey Bugaev <abugaev@oggettoweb.com>
+ */
+
+class Oggetto_Payment_RedirectController extends Mage_Core_Controller_Front_Action
+{
+    /**
+     * redirect action
+     *
+     * @return void
+     */
+    public function redirectAction()
+    {
+        echo 'AZZAZAZAZAZ';
+    }
+}
